fa095da00a6f99847ed922c0ae15f6f880cf5d7c
Ashley Thompson <ashleythompsonbca@gmail.com>
2020-04-21 01:49:40 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,11 +2,11 @@ def display_board(board)
   #board=[ " ", " ", " ", " ", " " , " ", " ", " ", " "]
   divider="|"
   lines=("-----------")
-  puts " #{board[0]} #{divider} #{board[1]} #{divider} #{board[2]}"
+  puts " #{board[0]} #{divider} #{board[1]} #{divider} #{board[2]} "
   puts "#{lines}"
-  puts "#{board[3]}  #{divider} #{board[4]} #{divider}  #{board[5]}"
+  puts " #{board[3]} #{divider} #{board[4]} #{divider} #{board[5]} "
   puts "#{lines}"
-  puts "#{board[6]}  #{divider} #{board[7]} #{divider}  #{board[8]}"
+  puts " #{board[6]} #{divider} #{board[7]} #{divider} #{board[8]} "
   # puts board
   #board[4]="X"
   #print board[4]="X"
